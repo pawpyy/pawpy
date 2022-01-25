c36fc9d631c385dab4c2a2b5909aed4e155d8096
박소영 <74605283+sso0711@users.noreply.github.com>
2022-01-25 18:07:54 +0900

    Delete pawpy directory

--- pawpy/user/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from user.models import user,pet
-from django.contrib.auth.admin import UserAdmin
-
-# Register your models here.
-admin.site.register(user)
-admin.site.register(pet)
